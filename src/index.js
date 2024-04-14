797404635e89240f30275ef516d2654dbbac5c00
Devesh <dp.devesh65@gmail.com>
2024-04-14 10:35:47 +0530

    index 2nd

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AboutSec from './AboutSec.jsx';
-import Contact from './Contact.jsx';
+import AboutSec from './AboutSec.jsx'
+import Contact from './Contact.jsx'
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/AboutSec" component={AboutSec} />
-        <Route path="/Contact" component={Contact} />
-      </Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/AboutSec" component={AboutSec} />
+      <Route path="/Contact" component={Contact} />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
