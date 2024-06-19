c1915f1134ad85022e0914f622f0488028018f8c
Devesh Pandey <132568340+deveshpandey65@users.noreply.github.com>
2024-06-19 14:58:48 +0530

    Update Projectdis.jsx

--- src/Projectdis.jsx
+++ src/Projectdis.jsx
@@ -3,12 +3,23 @@ import image2 from './img/Screenshot 2024-01-21 140150.png'
 import image3 from './img/Screenshot 2024-04-11 134109.png'
 import image4 from './img/Screenshot 2024-03-29 102934.png'
 import image5 from './img/Screenshot 2024-04-11 131733.png'
+import image6 from './img/Screenshot 2024-05-21 173139.png'
+import image7 from './img/Screenshot 2024-04-11 131733.png'
+import image9 from './img/Screenshot 2024-05-08 224240.png'
+import image10 from './img/Screenshot 2024-05-08 225112.png'
 
 
 
 
-export const Projectdis = [{ src: `${image1}`, text: 'Thinklay A Ecom Website', link:'https://thinklay.com/' },
-    { src: `${image2}`, text: 'Travell Entusiast App', link: 'https://thinklay.com/' },
+
+
+export const Projectdis = [
+    { src: `${image6}`, text: 'Envato-Market', link: 'https://deveshpandey65.github.io/envatomarket/' }, 
+    { src: `${image6}`, text: 'Internshala Automation', link: 'https://github.com/deveshpandey65/Internshala_Automation' },
+    { src: `${image1}`, text: 'Thinklay', link: 'https://thinklay.com/' }, 
+    { src: `${image9}`, text: 'Flipkart Clone', link:'https://deveshpandey65.github.io/FlipkartClone/' },
+    { src: `${image10}`, text: 'URL Shorter', link: 'https://github.com/deveshpandey65/URL_Shorter' },
+    { src: `${image2}`, text: 'Travell Entusiast App', link: 'https://github.com/deveshpandey65/Travell' },
     { src: `${image3}`, text: 'A Ecom App Using React', link: 'https://github.com/deveshpandey65/Ecom-React' },
-    { src: `${image4}`, text: 'Stone Paper Sessior Using React', link: 'https://thinklay.com/' },
+    { src: `${image4}`, text: 'Stone Paper Sessior Using React', link: 'https://github.com/deveshpandey65/StoneGame' },
     { src: `${image5}`, text: 'To Do List Using React', link: 'https://github.com/deveshpandey65/To-do-List-' }]
